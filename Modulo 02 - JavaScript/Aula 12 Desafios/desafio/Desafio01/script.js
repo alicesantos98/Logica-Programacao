39ea1c420b14d730688ac07e3664290e261c1c1e
alicesantos98 <alicealves060211@gmail.com>
2025-09-24 09:21:05 -0300

    a

--- Modulo 02 - JavaScript/Aula 12 Desafios/desafio/Desafio01/script.js
+++ Modulo 02 - JavaScript/Aula 12 Desafios/desafio/Desafio01/script.js
@@ -1,22 +1,9 @@
-// Desafio 1
-
 function calcularMedia() {
-    // pega os valores digitados nos campos
-  let n1 = parseFloat(document.getElementById("nota1").value);
-  let n2 = parseFloat(document.getElementById("nota2").value);
- 
-  // verifica se os valores são válidos
-  if (isNaN(n1) || isNaN(n2)) {
-    document.getElementById("resultado").innerText = "Por favor, digite as duas notas!";
-    return;
-  }
- 
-  // calcula a média
-  let media = (n1 + n2) / 2;
- 
-  // exibe dentro do parágrafo
-  document.getElementById("resultado").innerText = "A média é: " + media.toFixed(2);
-}
-    let nota1 = parseFloat (nota1)
-    let nota2 = parseFloat (nota2)
 
+  let nota1 = parseFloat(document.getElementById ("nota1").value)
+  let nota2 = parseFloat(document.getElementById ("nota2").value)
+
+  let media = (nota1 + nota2)/2
+
+  document.getElementById("resultado").innerText = "A resposta é: "+ media.toFixed(2); }
+  
--- Modulo 02 - JavaScript/Aula 12 Desafios/desafio/script.js
+++ Modulo 02 - JavaScript/Aula 12 Desafios/desafio/script.js
@@ -1,1 +1,9 @@
-function
+function esconder() {
+    let caixa = document.getElementById("texto")
+    caixa.style.display = "none"
+}
+
+function mostrar() {
+    let caixa = document.getElementById("texto")
+    caixa.style.display = "block"
+}
